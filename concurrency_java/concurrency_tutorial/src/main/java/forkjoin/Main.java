5c751baa371f04cce28b3f37189e1d75f65dd546
mehikmat <me.hemant.available@gmail.com>
2016-02-04 19:09:09 +0545

    adding fork-join java 7 feature as like map-reduce

---
+++ concurrency_java/concurrency_tutorial/src/main/java/forkjoin/Main.java
@@ -0,0 +1,11 @@
+package forkjoin;
+
+/**
+ * Created by hdhamee on 2/4/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int nThreads = Runtime.getRuntime().availableProcessors();
+        System.out.println(nThreads);
+    }
+}
